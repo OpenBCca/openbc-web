c9314d369a62450cf88f62c0cc3161d51b7a772f
garenpham <tanphamminh2002@gmail.com>
2023-10-18 18:06:27 -0700

    nextjs static

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
+  basePath: isProd ? '/openbc-web' : '',
   output: 'export',
   distDir: 'dist',
   images: {
