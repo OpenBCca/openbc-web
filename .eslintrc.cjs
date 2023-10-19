c5b95900c9e699f6adc77ed5f05bf4edf1270dba
garenpham <tanphamminh2002@gmail.com>
2023-10-18 17:53:33 -0700

    nextjs static

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,46 +1,46 @@
-module.exports ={
-  "extends": [
-    "next/core-web-vitals",
-    "eslint:recommended",
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:@typescript-eslint/strict-type-checked',
-    'plugin:@typescript-eslint/stylistic'
+    'plugin:@typescript-eslint/stylistic',
   ],
-  "parser": "@typescript-eslint/parser",
-  "plugins": ["@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   parserOptions: {
     project: './tsconfig.json',
     tsconfigRootDir: __dirname,
   },
-  "root": true,
-  "rules": {
-    "@typescript-eslint/naming-convention": ["error",{
-      "selector": "variable",
-      "types": ["boolean"],
-      "format": ["PascalCase"],
-      "prefix": ["is", "should", "has", "can", "did", "will"]
-    },
-    {
-      "selector": "variable",
-      "modifiers": ["destructured"],
-      "format": null
-    },
-    {
-      selector: 'typeLike',
-      format: ['PascalCase'],
-    },],
-    "@typescript-eslint/array-type": "error",
-    "@typescript-eslint/consistent-generic-constructors": "error",
-    "@typescript-eslint/no-confusing-void-expression": "error",
-    "@typescript-eslint/adjacent-overload-signatures": "error",
-    "@typescript-eslint/await-thenable": "error",
-    "@typescript-eslint/consistent-type-exports": ["error",{fixMixedExportsWithInlineTypeSpecifier: true}],
-    "@typescript-eslint/no-duplicate-type-constituents": "error",
-    "@typescript-eslint/no-empty-interface": "error",
-    "@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
-    "@typescript-eslint/no-unnecessary-type-assertion": "error",
-    "@typescript-eslint/no-unsafe-assignment": "error",
-    "@typescript-eslint/no-unsafe-return": "error",
-    "@typescript-eslint/no-useless-empty-export": "error"
-  }
+  root: true,
+  rules: {
+    'no-empty-pattern': 'warn',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'variable',
+        types: ['boolean'],
+        format: ['PascalCase'],
+        prefix: ['is', 'should', 'has', 'can', 'did', 'will'],
+      },
+      {
+        selector: 'variable',
+        modifiers: ['destructured'],
+        format: null,
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+    ],
+    '@typescript-eslint/consistent-type-definitions': 'warn',
+    '@typescript-eslint/no-unsafe-assignment': 'warn',
+    '@typescript-eslint/no-unsafe-call': 'warn',
+    '@typescript-eslint/no-unsafe-member-access': 'warn',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    '@typescript-eslint/no-unnecessary-condition': 'warn',
+    '@typescript-eslint/ban-types': 'warn',
+    '@typescript-eslint/no-empty-interface': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
 }
