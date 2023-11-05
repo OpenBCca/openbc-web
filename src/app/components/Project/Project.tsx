20c8444f76c45752a93cb0c7b3b4f8a7f1673985
CorreyL <correylim@gmail.com>
2023-11-05 15:23:54 -0800

    Add interface for component props

--- src/app/components/Project/Project.tsx
+++ src/app/components/Project/Project.tsx
@@ -1,6 +1,29 @@
 import { } from 'react';
 
-export default function Project() {
+interface Link {
+  title: string;
+  url: string;
+}
+
+interface ProjectProps {
+  title: string;
+  description: string;
+  links: Array<Link>;
+  partner?: string;
+  languages: Array<string>;
+  technologies?: Array<string>;
+  programAreas?: string;
+}
+
+export default function Project({
+  title,
+  description,
+  links,
+  partner,
+  languages,
+  technologies,
+  programAreas,
+}: ProjectProps) {
   return (
     <div>Hello World</div>
   );
