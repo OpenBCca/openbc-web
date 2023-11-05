71b1dabbc12e1bba15ef5515fd84627647ac7475
PooriaT <pooria_taghdiri@hotmail.com>
2023-11-05 12:13:17 -0800

    Adding github_clinet to app -> clientService -> clientIntegration

---
+++ src/app/clientService/clientIntegration/github_clinet.tsx
@@ -0,0 +1,48 @@
+import axios, { AxiosResponse } from 'axios';
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+// Your GitHub personal access token
+// Replace 'YOUR_PERSONAL_ACCESS_TOKEN' with your GitHub personal access token
+const accessToken: string | undefined = process.env.GITHUB_TOKEN;
+
+if (!accessToken) {
+  throw new Error('GitHub access token is not provided.');
+}
+
+// Axios instance with headers
+const axiosInstance = axios.create({
+  baseURL: 'https://api.github.com',
+  headers: {
+    'Authorization': `token ${accessToken}`,
+    'Content-Type': 'application/json',
+  },
+});
+
+// Function to get the list of repositories
+const getReposList = async (): Promise<AxiosResponse> => {
+  try {
+    const response: AxiosResponse = await axiosInstance.get('/orgs/OpenBCca/repos');
+    return response;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+// Function to get repository info
+const getRepoInfo = async (repo_name: string, para: string = ''): Promise<AxiosResponse> => {
+  try {
+    const response: AxiosResponse = await axiosInstance.get(`/repos/OpenBCca/${repo_name}${para}`);
+    return response;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export {
+  getReposList,
+  getRepoInfo
+};
