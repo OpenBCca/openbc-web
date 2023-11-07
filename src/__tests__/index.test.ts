b622a4e442de3b658b7600d5585017c747a26b0a
Nam Anh Mai <maithenamanh@gmail.com>
2023-11-07 02:39:15 -0800

    First jest test

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,9 @@
+describe('true is true and false is false', () => {
+  test('true is true', () => {
+    expect(true).toBe(true)
+  })
+
+  test('false is false', () => {
+    expect(false).toBe(false)
+  })
+})
