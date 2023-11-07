8d760f8d5962e16adf3febc9c4cd15cabd4345b4
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2023-11-06 20:29:41 -0800

    Hardcode values for title, description, and style in Banner.tsx

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -3,35 +3,27 @@ import React from 'react'
 import { Box, Typography } from '@mui/material'
 
 interface BannerProps {
-  title?: string;
-  description?: string;
-  backgroundColor?: string;
-  height?: string;
+
 }
 
-function Banner({
-                  title = 'Be a Force For Good in BC!',
-                  description = 'Do you have design, writing, or coding skills? You have the power to elevate British Columbia to new heights\n' +
-                  '        through OpenBC.',
-                  backgroundColor = '#ededed',
-                  height = '15rem',
-                }: BannerProps) {
+function Banner() {
   return (
     <Box
       sx={{
         textAlign: 'center',
-        backgroundColor,
-        height,
+        backgroundColor: '#ededed',
+        height: '15rem',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
       }}
     >
       <Typography variant='h3'>
-        {title}
+        {'Be a Force For Good in BC!'}
       </Typography>
       <Typography variant='body1'>
-        {description}
+        {'Do you have design, writing, or coding skills? You have the power to elevate British Columbia to new heights\n' +
+          '        through OpenBC.'}
       </Typography>
     </Box>
   )
