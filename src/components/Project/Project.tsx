8a242b2ca12ac8f3c5542fec05d2901cc99e7404
CorreyL <correylim@gmail.com>
2023-11-09 21:13:47 -0800

    Linting changes

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,16 +1,8 @@
 'use client'
 
-import {
-  Box,
-  Card,
-  CardContent,
-  Link,
-  Typography,
-} from '@mui/material';
-import { } from 'react';
-import {
-  Project,
-} from './Project.types';
+import { Box, Card, CardContent, Link, Typography } from '@mui/material'
+import {} from 'react'
+import { Project } from './Project.types'
 
 export default function Project({
   title,
@@ -28,40 +20,24 @@ export default function Project({
           <Typography variant="h4" component="div">
             {title}
           </Typography>
-          <Typography variant="body2">
-            {description}
-          </Typography>
+          <Typography variant="body2">{description}</Typography>
           <Typography variant="h6">
-            {
-              links.map((link, idx) => (
-                <Link href={link.url} key={`project-link-${idx}`}>{link.title}</Link>
-              ))
-            }
+            {links.map((link, idx) => (
+              <Link href={link.url} key={`project-link-${idx}`}>
+                {link.title}
+              </Link>
+            ))}
           </Typography>
-          <Typography>
-            Partner: {partner}
-          </Typography>
-          <Typography>
-            Languages: {languages.join(", ")}
-          </Typography>
-          {
-            technologies
-            && (
-              <Typography>
-                Technologies: {technologies.join(", ")}
-              </Typography>
-            )
-          }
-          {
-            programAreas
-            && (
-              <Typography>
-                Program Areas: {programAreas}
-              </Typography>
-            )
-          }
+          <Typography>Partner: {partner}</Typography>
+          <Typography>Languages: {languages.join(', ')}</Typography>
+          {technologies && (
+            <Typography>Technologies: {technologies.join(', ')}</Typography>
+          )}
+          {programAreas && (
+            <Typography>Program Areas: {programAreas}</Typography>
+          )}
         </CardContent>
       </Card>
     </Box>
-  );
-};
+  )
+}
