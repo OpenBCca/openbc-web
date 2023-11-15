5eeca6b19a310855b7a8787c800c44392e46fd26
Nam Anh Mai <maithenamanh@gmail.com>
2023-11-15 01:30:50 -0800

    Add header snapshot test and its snapshot

--- src/__tests__/snapshot.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react'
-import Header from '@/components/Header/Header'
-
-it('renders header unchanged', () => {
-  const { container } = render(<Header />)
-  expect(container).toMatchSnapshot()
-})
