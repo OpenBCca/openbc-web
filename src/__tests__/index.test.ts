1224679ec9624acf181db5da6a2c4c07ede16e40
Nam Anh Mai <maithenamanh@gmail.com>
2023-11-15 23:48:23 -0800

    Deleted obsolete setup and test files

--- src/__tests__/index.test.ts
+++
@@ -1,9 +0,0 @@
-describe('true is true and false is false', () => {
-  test('true is true', () => {
-    expect(true).toBe(true)
-  })
-
-  test('false is false', () => {
-    expect(false).toBe(false)
-  })
-})
