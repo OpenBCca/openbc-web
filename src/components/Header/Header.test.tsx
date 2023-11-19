f6e64df9b8fa4d6c8e77ab9fbe9f046684cbd4b6
Nam Anh Mai <maithenamanh@gmail.com>
2023-11-18 18:15:50 -0800

    Remove noise comments

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -6,10 +6,7 @@ describe('Header', () => {
   it('should render header', () => {
     render(<Header />)
 
-    // Query header element
     const header = screen.getByRole('banner')
-
-    // Query the navigation anchor elements
     const projectNav = screen.getByRole('link', {
       name: 'Projects',
     })
@@ -20,10 +17,7 @@ describe('Header', () => {
       name: 'About',
     })
 
-    // Check if header is in the document
     expect(header).toBeInTheDocument()
-
-    // Check href for each anchor link
     expect(projectNav).toHaveAttribute('href', '')
     expect(joinUsNav).toHaveAttribute('href', '')
     expect(aboutNav).toHaveAttribute('href', '')
