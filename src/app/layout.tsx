4996c42f50db66aa565f891244cae2dcb91b3cf8
Sam Huo <hhuo213@hotmail.com>
2023-11-18 17:40:49 -0800

    Continue Rename

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import Header from '@/components/Header/Header';
+import Header from '@/components/Header/header';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
