8f32bd3f09199dae56977945aa40ae535958f41f
derekdbcit <dereksaviodsouza@gmail.com>
2023-11-20 22:32:34 -0800

    Projects updated + footer component added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,51 +1,53 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function About() {
   return (
     <>
-      <h1>About</h1>
-      <p className="description">Placeholder</p>
-      <br></br>
-      <h2>Our Founder</h2>
-      <Image
-        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-        alt="John Doe"
-        width={500}
-        height={500}
-        className="profile"
-      />
-      <p className="description">Placeholder</p>
-      <br></br>
-      <h2>Person 1</h2>
-      <Image
-        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-        alt="John Doe"
-        width={500}
-        height={500}
-        className="profile"
-      />
-      <p className="description">Placeholder</p>
-      <br></br>
-      <h2>Person 2</h2>
-      <Image
-        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-        alt="John Doe"
-        width={500}
-        height={500}
-        className="profile"
-      />
-      <p className="description">Placeholder</p>
-      <br></br>
-      <h2>Person 3</h2>
-      <Image
-        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-        alt="John Doe"
-        width={500}
-        height={500}
-        className="profile"
-      />
-      <p className="description">Placeholder</p>
-      <br></br>
+      <div className="centered-elements">
+        <h1>About</h1>
+        <p className="description">Placeholder</p>
+        <br></br>
+        <h2>Our Founder</h2>
+        <Image
+          src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+          alt="John Doe"
+          width={500}
+          height={500}
+          className="profile"
+        />
+        <p className="description">Placeholder</p>
+        <br></br>
+        <h2>Person 1</h2>
+        <Image
+          src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+          alt="John Doe"
+          width={500}
+          height={500}
+          className="profile"
+        />
+        <p className="description">Placeholder</p>
+        <br></br>
+        <h2>Person 2</h2>
+        <Image
+          src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+          alt="John Doe"
+          width={500}
+          height={500}
+          className="profile"
+        />
+        <p className="description">Placeholder</p>
+        <br></br>
+        <h2>Person 3</h2>
+        <Image
+          src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+          alt="John Doe"
+          width={500}
+          height={500}
+          className="profile"
+        />
+        <p className="description">Placeholder</p>
+        <br></br>
+      </div>
     </>
-  )
+  );
 }
