495770375a6558a75be0edadbb74afe6239afbeb
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2023-11-21 20:50:38 -0800

    Updating project-overview.tsx to center itself.

--- src/components/project-overview/project-overview.tsx
+++ src/components/project-overview/project-overview.tsx
@@ -1,4 +1,4 @@
-import { Card } from '@mui/material';
+import { Card, Container, Grid } from '@mui/material';
 import Project from '@/components/project/project';
 import { Link } from '@/app/dataModels/link';
 import { ProjectStatusTypes } from '@/app/dataModels/project';
@@ -11,17 +11,31 @@ const languagesForProjectOverview = ['typescript'];
 
 export function ProjectOverview() {
   return (
-    <Card>
-      <Project
-        title={'Project Overview'}
-        description={
-          'OpenBC is a volunteer-driven organization that brings together technologists, designers, data analysts, and community members to work on projects that address civic and social challenges in BC using technology and innovation. They host hackathons, workshops, and events where participants collaborate to create solutions for local issues.'
-        }
-        links={linksForProjectOverview}
-        languages={languagesForProjectOverview}
-        status={ProjectStatusTypes.active}
-        location={'Remote'}
-      />
-    </Card>
+    <Grid
+      container
+      spacing={0}
+      alignItems="center"
+      justifyContent="center"
+      sx={{ backgroundColor: '#ededed'}}
+    >
+      <Grid item>
+        <Card
+          sx={{
+            maxWidth: 700,
+          }}
+        >
+          <Project
+            title={'Project Overview'}
+            description={
+              'OpenBC is a volunteer-driven organization that brings together technologists, designers, data analysts, and community members to work on projects that address civic and social challenges in BC using technology and innovation. They host hackathons, workshops, and events where participants collaborate to create solutions for local issues.'
+            }
+            links={linksForProjectOverview}
+            languages={languagesForProjectOverview}
+            status={ProjectStatusTypes.active}
+            location={'Remote'}
+          />
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
