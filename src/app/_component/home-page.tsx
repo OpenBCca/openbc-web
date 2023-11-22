5516915d047f382ec876b866c84d762ed98eaa09
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2023-11-21 19:57:47 -0800

    Added project-overview.tsx component and updated project.ts data model to include location and status.

--- src/app/_component/home-page.tsx
+++ src/app/_component/home-page.tsx
@@ -1,9 +1,11 @@
 import Banner from '@/components/banner/banner';
+import { ProjectOverview } from '@/components/project-overview/project-overview';
 
 export function HomePage() {
   return (
     <div>
       <Banner />
+      <ProjectOverview />
     </div>
   );
 }
