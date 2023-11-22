2c1dca0f147709b89863a4fb4e6a28f29f978390
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2023-11-21 20:51:05 -0800

    Added Margin

--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -5,6 +5,9 @@ import React from 'react';
 import { Link as LinkInterface } from '../../app/dataModels/link';
 import { Project as ProjectInterface } from '../../app/dataModels/project';
 
+const moreInformationStyles = {
+  marginTop: '1% 0',
+};
 export default function Project({
   title,
   description,
@@ -17,29 +20,59 @@ export default function Project({
   status,
 }: ProjectInterface) {
   return (
-    <Box>
+    <Box sx={{ fontFamily: 'inherit' }}>
       <Card sx={{ minWidth: 275 }}>
         <CardContent>
-          <Typography variant="h4" component="div">
+          <Typography
+            variant="h4"
+            component="div"
+            sx={{ fontFamily: 'inherit', fontWeight: '600' }}
+          >
             {title}
           </Typography>
-          <Typography variant="body2">{description}</Typography>
-          {status && <Typography variant="body1">{status}</Typography>}
-          {location && <Typography variant="body1">{location}</Typography>}
-          <Typography variant="h6">
+          <Typography variant="body2" sx={{ fontFamily: 'inherit' }}>
+            {description}
+          </Typography>
+          {status && (
+            <Typography
+              variant="body2"
+              component="p"
+              mt={0.5}
+            >{`Status: ${status}`}</Typography>
+          )}
+          {location && (
+            <Typography
+              variant="body2"
+              component="p"
+              mt={0.5}
+            >{`Location: ${location}`}</Typography>
+          )}
+          <Typography variant="body2" mt={0.5}>
+            Links:&nbsp;
             {links.map((link: LinkInterface, idx: number) => (
               <Link href={link.url} key={`project-link-${idx}`}>
                 {link.title}
               </Link>
             ))}
           </Typography>
-          <Typography>Partner: {partner}</Typography>
-          <Typography>Languages: {languages.join(', ')}</Typography>
+          {partner && (
+            <Typography variant="body2" mt={0.5}>
+              {' '}
+              Partner: {partner}
+            </Typography>
+          )}
+          <Typography variant="body2" mt={0.5}>
+            Languages: {languages.join(', ')}
+          </Typography>
           {technologies && (
-            <Typography>Technologies: {technologies.join(', ')}</Typography>
+            <Typography mt={0.5}>
+              Technologies: {technologies.join(', ')}
+            </Typography>
           )}
           {programAreas && (
-            <Typography>Program Areas: {programAreas}</Typography>
+            <Typography variant="body2" mt={0.5}>
+              Program Areas: {programAreas}
+            </Typography>
           )}
         </CardContent>
       </Card>
