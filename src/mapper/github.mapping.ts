c9ea809e281df8a92bcaf60eddf1499d48e3187a
PooriaT <pooria_taghdiri@hotmail.com>
2023-11-23 21:14:53 -0800

    Refactoring the code

--- src/mapper/github.mapping.ts
+++ src/mapper/github.mapping.ts
@@ -1,58 +1,41 @@
 import { getReposList, getRepoInfo } from '../client-services/github.client-service'
 
-interface RepoInfo {
+interface Repos {
+    name: string;
+  }
+  
+  interface RepoInfo {
     name: string;
     description?: string;
     url: string;
     languages: string[];
     language: string;
-    contributors: { 
-        name: string; 
-        avatarUrl: string 
-    }[];
-}
-
-export async function map(): Promise<RepoInfo[]> {
+    contributors: { name: string; avatarUrl: string }[];
+  }
+  
+  async function mapRepoInfo(repo: Repos): Promise<RepoInfo> {
+    const repoInfoResponse = await getRepoInfo(repo.name);
+    const data = repoInfoResponse.data;
+  
+    const languagesResponse = await getRepoInfo(repo.name, '/languages');
+    const contributorsResponse = await getRepoInfo(repo.name, '/contributors');
+  
+    return {
+      name: repo.name,
+      description: data.description,
+      url: data.url,
+      languages: Object.keys(languagesResponse.data),
+      language: data.language,
+      contributors: contributorsResponse.data.map((contributor: any) => ({
+        name: contributor.login,
+        avatarUrl: contributor.avatarUrl,
+      })),
+    };
+  }
+  
+  async function githubMapper(): Promise<RepoInfo[]> {
     const reposListResponse = await getReposList();
-    const repos: string[] = [];
-    const repoInfo: { 
-        name: string; 
-        description?: string; 
-        url: string; languages: string[]; 
-        language: string; 
-        contributors: { 
-            name: string; 
-            avatarUrl: string 
-        }[]; 
-    }[] = [];
-
-    for (let i = 0; i < reposListResponse.data.length; i++){
-        repos.push(reposListResponse.data[i].name);
-        repoInfo.push({ name: reposListResponse.data[i].name } as RepoInfo);
-    }
-
-    for (let i = 0; i < repos.length; i++){
-        const repoInfoResponse = await getRepoInfo(repos[i]);
-        const data = repoInfoResponse.data;
-
-        repoInfo[i].description = data.description;
-        repoInfo[i].url = data.url;
-
-        const languagesResponse = await getRepoInfo(repos[i], '/languages');
-        repoInfo[i].languages = Object.keys(languagesResponse.data);
-        repoInfo[i].language = data.language;
-
-        const contributorsResponse = await getRepoInfo(repos[i], '/contributors');
-        const allContributors: { 
-            name: string; 
-            avatarUrl: string 
-        }[] = [];
-
-        for (const contributor of contributorsResponse.data) {
-            allContributors.push({ name: contributor.login, avatarUrl: contributor.avatarUrl });
-        }
-        repoInfo[i].contributors = allContributors;
-    }
-    return repoInfo;
-
-}
+    const repos: Repos[] = reposListResponse.data.map((repo: any) => ({ name: repo.name }));
+    const repoPromises = await repos.map(mapRepoInfo);
+    return Promise.all(repoPromises);
+  }
