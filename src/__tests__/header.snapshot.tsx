452ff9760f62351e70bf71a404cb2f84617d2094
Nam Anh Mai <maithenamanh@gmail.com>
2023-11-24 14:09:58 -0800

    Added semicolon to snapshot test and updated snapshot

--- src/__tests__/header.snapshot.tsx
+++ src/__tests__/header.snapshot.tsx
@@ -1,12 +1,12 @@
-import Header from '@/components/Header/Header'
-import { render } from '@testing-library/react'
+import Header from '@/components/header/header';
+import { render } from '@testing-library/react';
 
 // Use this if you want to skip this test
-describe.skip('skips snapshot test', () => {})
+describe.skip('skips snapshot test', () => {});
 
 describe('Header snapshot test', () => {
   it('should render header unchanged', () => {
-    const { container } = render(<Header />)
-    expect(container).toMatchSnapshot()
-  })
-})
+    const { container } = render(<Header />);
+    expect(container).toMatchSnapshot();
+  });
+});
