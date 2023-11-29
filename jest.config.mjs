6345b05d53bc51d3b37e27ca2ed597c1138ee426
Yang Su <yangsu.personal@gmail.com>
2023-11-28 22:04:06 -0800

    add jest coverage and test pipeline

--- jest.config.mjs
+++ jest.config.mjs
@@ -14,6 +14,12 @@ const config = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      lines: 100
+    }
+  }
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
