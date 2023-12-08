5c37e2e413b18dc26f9a2a266a5bcf3a26bcc0a2
derekdbcit <dereksaviodsouza@gmail.com>
2023-12-07 23:23:46 -0800

    Join Us Inputs Created

---
+++ src/app/join-us/page.tsx
@@ -0,0 +1,13 @@
+import JoinUsForm from '@/components/join-us-form/join-us-form';
+import React from 'react';
+
+export default function JoinUs() {
+  return (
+    <>
+      <div className="centered-elements">
+        <h1>Join Us</h1>
+      </div>
+      <JoinUsForm />
+    </>
+  );
+}
---
+++ src/components/join-us-form/join-us-form.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { Box, Button, TextField } from '@mui/material';
+
+function JoinUsForm() {
+  // Function to prevent form submission on Enter key press
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          width: '100%',
+          maxWidth: 400,
+          mt: 2,
+        }}
+      >
+        <TextField
+          label="Full Name"
+          sx={{ width: '150%', mb: 2 }}
+          autoComplete="none"
+          required
+        />
+        <TextField
+          label="Email"
+          sx={{ width: '150%', mb: 2 }}
+          autoComplete="none"
+          required
+        />
+        <TextField
+          label="Discord Account"
+          sx={{ width: '150%', mb: 2 }}
+          autoComplete="none"
+          required
+        />
+        <TextField
+          label="GitHub Account"
+          sx={{ width: '150%', mb: 2 }}
+          autoComplete="none"
+          required
+        />
+        <TextField
+          label="Message"
+          // minimum width for the multiline input
+          sx={{ width: '150%', minWidth: 200, mb: 2 }}
+          multiline
+          rows={5}
+          maxRows={5}
+          autoComplete="none"
+          required
+        />
+      </Box>
+      <Button type="submit">Submit</Button>
+    </form>
+  );
+}
+
+export default JoinUsForm;
