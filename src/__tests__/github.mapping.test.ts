654077966296be9e77043afaa364776287f69419
PooriaT <pooria_taghdiri@hotmail.com>
2023-12-08 18:38:33 -0800

    GitHub mapper + unit test

---
+++ src/__tests__/github.mapping.test.ts
@@ -0,0 +1,99 @@
+import { mapProject, mapProjects } from '../mapper/github.mapping';
+
+describe('mapProject', () => {
+  it('should map a repository object to a project object', () => {
+    const repository = {
+      name: 'test-repo',
+      description: 'This is a test repository.',
+      url: 'https://github.com/test/test-repo',
+      language: 'Python',
+    };
+    const languages = ['Python', 'JavaScript'];
+    const contributors = [
+      { login: 'johndoe', avatarUrl: 'https://example.com/avatar.png' },
+    ];
+
+    const project = mapProject(repository, languages, contributors);
+
+    expect(project).toEqual({
+      name: 'test-repo',
+      description: 'This is a test repository.',
+      url: 'https://github.com/test/test-repo',
+      language: 'Python',
+      contributors: [
+        { name: 'johndoe', avatarUrl: 'https://example.com/avatar.png' },
+      ],
+      languages: ['Python', 'JavaScript'],
+    });
+  });
+});
+
+describe('mapProjects', () => {
+  it('should map an array of repository objects to an array of project objects', () => {
+    const repositories = [
+      {
+        name: 'test-repo1',
+        description: 'This is the first test repository.',
+        url: 'https://github.com/test/test-repo1',
+        language: 'Python',
+      },
+      {
+        name: 'test-repo2',
+        description: 'This is the second test repository.',
+        url: 'https://github.com/test/test-repo2',
+        language: 'JavaScript',
+      },
+    ];
+    const repositoriesLanguages = {
+      'test-repo1': ['Python', 'JavaScript'],
+      'test-repo2': ['JavaScript', 'CSS', 'HTML'],
+    };
+    const repositoriesContributors = {
+      'test-repo1': [
+        { login: 'janedoe', avatarUrl: 'https://example.com/avatar2.png' },
+      ],
+      'test-repo2': [
+        {
+          login: 'johndoe',
+          avatarUrl: 'https://example.com/johndoe_avatar.png',
+        },
+      ],
+    };
+    const repositoriesLanguagesArray = Object.values(repositoriesLanguages);
+    const repositoriesContributorsArray = Object.values(
+      repositoriesContributors
+    );
+
+    const projects = mapProjects(
+      repositories,
+      repositoriesLanguagesArray,
+      repositoriesContributorsArray
+    );
+
+    expect(projects).toEqual([
+      {
+        name: 'test-repo1',
+        description: 'This is the first test repository.',
+        url: 'https://github.com/test/test-repo1',
+        language: 'Python',
+        contributors: [
+          { name: 'janedoe', avatarUrl: 'https://example.com/avatar2.png' },
+        ],
+        languages: ['Python', 'JavaScript'],
+      },
+      {
+        name: 'test-repo2',
+        description: 'This is the second test repository.',
+        url: 'https://github.com/test/test-repo2',
+        language: 'JavaScript',
+        contributors: [
+          {
+            name: 'johndoe',
+            avatarUrl: 'https://example.com/johndoe_avatar.png',
+          },
+        ],
+        languages: ['JavaScript', 'CSS', 'HTML'],
+      },
+    ]);
+  });
+});
--- src/mapper/github.mapping.ts
+++ src/mapper/github.mapping.ts
@@ -1,46 +1,31 @@
-import {
-  getRepoInfo,
-  getReposList,
-} from '../client-services/github.client-service'
-
-interface Repos {
-  name: string
-}
-
-// interface RepoInfo {
-//   name: string
-//   description?: string
-//   url: string
-//   languages: string[]
-//   language: string
-//   contributors: { name: string; avatarUrl: string }[]
-// }
-
-export async function mapRepoInfo(repo: Repos) {
-  const repoInfoResponse = await getRepoInfo(repo.name)
-  const data = repoInfoResponse.data
-
-  const languagesResponse = await getRepoInfo(repo.name, '/languages')
-  const contributorsResponse = await getRepoInfo(repo.name, '/contributors')
-
+export function mapProject(
+  repository: any,
+  languages: string[],
+  contributors: any[]
+) {
   return {
-    name: repo.name,
-    description: data.description,
-    url: data.url,
-    languages: Object.keys(languagesResponse.data),
-    language: data.language,
-    contributors: contributorsResponse.data.map((contributor: any) => ({
+    name: repository.name,
+    description: repository.description,
+    url: repository.url,
+    language: repository.language,
+    contributors: contributors.map((contributor: any) => ({
       name: contributor.login,
       avatarUrl: contributor.avatarUrl,
     })),
-  }
+    languages: languages,
+  };
 }
 
-export function mapProjects(repositories: any[]) {
-  const reposListResponse = getReposList()
-  const repos: Repos[] = reposListResponse.data.map((repo: any) => ({
-    name: repo.name,
-  }))
-  const repoPromises = repos.map(mapRepoInfo)
-  return Promise.all(repoPromises)
+export function mapProjects(
+  repositories: any[],
+  repositoriesLanguages: any[],
+  repositoriesContributors: any[]
+) {
+  return repositories.map((repository: any) =>
+    mapProject(
+      repository,
+      repositoriesLanguages[repository.name],
+      repositoriesContributors[repository.name]
+    )
+  );
 }
