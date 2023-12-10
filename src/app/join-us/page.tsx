679dd228843dafad3220493c32e2442069ef4c9a
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2023-12-09 18:41:18 -0800

    Added more content.

--- src/app/join-us/page.tsx
+++ src/app/join-us/page.tsx
@@ -1,31 +1,57 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, List, ListItem, Link } from '@mui/material';
 
 export default function JoinUs() {
   return (
-    <Box>
-      <Typography
-        variant="h5"
-        component="div"
-        sx={{ fontFamily: 'inherit', fontWeight: '700' }}
-      >
-        Join Us
-      </Typography>
-      <Typography variant="body2">
-        {'Ready to contribute to BC through Open source projects? Join us on the ' +
-          'OpenBC Discord and become a part of our active and welcoming community.' +
-          "Here's how you can engage:"}
-      </Typography>
-      <ul>
-        <li>Say hi in our #introduce-yourself channel!</li>
-        <li>
-          Ask our Support Squad a question in our #support-threads channel!
-        </li>
-        <li>
-          Let us know what projects you're interested in discussing in our
-          #showcase channel!
-        </li>
-      </ul>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        height: '100vh',
+        marginTop: '1rem',
+      }}
+    >
+      <Box sx={{ maxWidth: 600, textAlign: 'left' }}>
+        <Typography
+          variant="h5"
+          component="div"
+          fontWeight="bold"
+          textAlign={'center'}
+        >
+          Join Us
+        </Typography>
+        <Typography variant="body2">
+          Ready to contribute to BC through Open source projects? Join us on the{' '}
+          <Link
+            href="https://discord.gg/JYr2EyEw"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            OpenBC Discord
+          </Link>{' '}
+          and become a part of our active and welcoming community. Here's how
+          you can engage:
+        </Typography>
+        <List>
+          <ListItem>
+            <Typography variant="body2">
+              Say hi in our #introduce-yourself channel!
+            </Typography>
+          </ListItem>
+          <ListItem>
+            <Typography variant="body2">
+              Ask our Support Squad a question in our #support-threads channel!
+            </Typography>
+          </ListItem>
+          <ListItem>
+            <Typography variant="body2">
+              Let us know what projects you're interested in discussing in our
+              #showcase channel!
+            </Typography>
+          </ListItem>
+        </List>
+      </Box>
     </Box>
   );
 }
