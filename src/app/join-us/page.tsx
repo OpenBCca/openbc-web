bd7e950b33f737e0c06b8cbe7b6ee0303c200f4c
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2023-12-22 15:33:37 -0800

    rolled backed join us page to main.

--- src/app/join-us/page.tsx
+++ src/app/join-us/page.tsx
@@ -1,57 +1,11 @@
 import React from 'react';
-import { Box, Typography, List, ListItem, Link } from '@mui/material';
 
 export default function JoinUs() {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        height: '100vh',
-        marginTop: '1rem',
-      }}
-    >
-      <Box sx={{ maxWidth: 600, textAlign: 'left' }}>
-        <Typography
-          variant="h5"
-          component="div"
-          fontWeight="bold"
-          textAlign={'center'}
-        >
-          Join Us
-        </Typography>
-        <Typography variant="body2">
-          Ready to contribute to BC through Open source projects? Join us on the{' '}
-          <Link
-            href="https://discord.gg/JYr2EyEw"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            OpenBC Discord
-          </Link>{' '}
-          and become a part of our active and welcoming community. Here&apos;s
-          how you can engage:
-        </Typography>
-        <List>
-          <ListItem>
-            <Typography variant="body2">
-              Say hi in our #introduce-yourself channel!
-            </Typography>
-          </ListItem>
-          <ListItem>
-            <Typography variant="body2">
-              Ask our Support Squad a question in our #support-threads channel!
-            </Typography>
-          </ListItem>
-          <ListItem>
-            <Typography variant="body2">
-              Let us know what projects you&apos;re interested in discussing in
-              our #showcase channel!
-            </Typography>
-          </ListItem>
-        </List>
-      </Box>
-    </Box>
+    <>
+      <div className="centered-elements">
+        <h1>Join Us</h1>
+      </div>
+    </>
   );
 }
