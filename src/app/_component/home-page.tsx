a5a1ef7c55a6f035cb0687dce5443aec12da23fe
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2023-12-21 20:41:11 -0800

    Changed header tests to work with new header.

--- src/app/_component/home-page.tsx
+++ src/app/_component/home-page.tsx
@@ -1,11 +1,9 @@
 import Banner from '@/components/banner/banner';
-import { ProjectOverview } from '@/components/project-overview/project-overview';
 
 export function HomePage() {
   return (
     <div>
       <Banner />
-      <ProjectOverview />
     </div>
   );
 }
