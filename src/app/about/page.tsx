7019cfd2a2aa723ab5db15c76df9aa1addb6c2a5
derekdbcit <dereksaviodsouza@gmail.com>
2023-12-26 11:46:39 -0800

    Revert "Header/footer added back to About"
    This reverts commit fe72810e2b4a468622a3391263f7524d94991e29.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,47 +2,39 @@ import React from 'react';
 import { Typography } from '@mui/material';
 import Container from '@mui/material/Container';
 import './about.scss';
-import Header from '@/components/header/header';
-import Footer from '@/components/footer/footer';
 
 export default function About() {
   return (
-    <>
-      <Header />
-      <Container maxWidth="lg" className="container">
-        <Typography className="about-header" variant="h3">
-          About
-        </Typography>
-        <Typography variant="body2" paragraph>
-          OpenBC, a community-driven initiative dedicated to harnessing the
-          power of technology, collaboration, and innovation to create positive
-          impacts across British Columbia, Canada. Our mission is to bring
-          together individuals passionate about technology, civic engagement,
-          and community development to address the unique challenges and
-          opportunities faced by our province.
-        </Typography>
-        <Typography variant="body2" paragraph>
-          <strong>
-            Utilize Your Skills for the Betterment of British Columbia with
-            OpenBC
-          </strong>
-        </Typography>
-        <Typography variant="body2" paragraph>
-          Unlock the power of your skills and expertise to drive positive change
-          in British Columbia through OpenBC. At OpenBC, we&apos;re passionate
-          about leveraging your unique abilities for the advancement of our
-          province. Whether you&apos;re a tech enthusiast, a creative visionary,
-          or a community leader, your skills can play a crucial role in
-          addressing challenges and creating opportunities.
-        </Typography>
-        <Typography variant="body2">
-          <strong>
-            Contribute to Open Source Projects for BC, from Anywhere, with
-            OpenBC
-          </strong>
-        </Typography>
-      </Container>
-      <Footer />
-    </>
+    <Container maxWidth="lg" className="container">
+      <Typography className="about-header" variant="h3">
+        About
+      </Typography>
+      <Typography variant="body2" paragraph>
+        OpenBC, a community-driven initiative dedicated to harnessing the power
+        of technology, collaboration, and innovation to create positive impacts
+        across British Columbia, Canada. Our mission is to bring together
+        individuals passionate about technology, civic engagement, and community
+        development to address the unique challenges and opportunities faced by
+        our province.
+      </Typography>
+      <Typography variant="body2" paragraph>
+        <strong>
+          Utilize Your Skills for the Betterment of British Columbia with OpenBC
+        </strong>
+      </Typography>
+      <Typography variant="body2" paragraph>
+        Unlock the power of your skills and expertise to drive positive change
+        in British Columbia through OpenBC. At OpenBC, we&apos;re passionate
+        about leveraging your unique abilities for the advancement of our
+        province. Whether you&apos;re a tech enthusiast, a creative visionary,
+        or a community leader, your skills can play a crucial role in addressing
+        challenges and creating opportunities.
+      </Typography>
+      <Typography variant="body2">
+        <strong>
+          Contribute to Open Source Projects for BC, from Anywhere, with OpenBC
+        </strong>
+      </Typography>
+    </Container>
   );
 }
