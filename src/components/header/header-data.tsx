e8e6e86d46cf9c61ae83c1633721d1966eb8e73c
derekd03 <134988441+derekd03@users.noreply.github.com>
2024-01-02 20:20:55 -0800

    Refactored header to use prop value, plus components to be client side - origin/features/navigation (#50)
    Co-authored-by: Yang Su <yangsu.personal@gmail.com>

---
+++ src/components/header/header-data.tsx
@@ -0,0 +1,9 @@
+import HeaderEnum from '../../enum/header-enum';
+
+const headerData = [
+  { label: 'Projects', value: HeaderEnum.projects, href: '/projects' },
+  { label: 'Join Us', value: HeaderEnum.joinUs, href: '/join-us' },
+  { label: 'About', value: HeaderEnum.about, href: '/about' },
+];
+
+export default headerData;
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,42 +1,49 @@
 'use client';
+
+import headerData from '@/components/header/header-data';
 import { Box, Link, Tab, Tabs } from '@mui/material';
-import React from 'react';
+import { usePathname } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import HeaderEnum from '../../enum/header-enum';
+import generateHeaderData from '../../utils/header-map/header-map';
 import './header.scss';
 
 function Header() {
+  const pathToEnumMap = generateHeaderData();
+  const pathname = usePathname();
+  const [currentPath, setCurrentPath] = useState(
+    pathToEnumMap[pathname] || false
+  );
+
+  useEffect(() => {
+    setCurrentPath(pathToEnumMap[pathname] || false);
+  }, [pathToEnumMap, pathname]);
+
+  const handleChange = (
+    event: React.ChangeEvent<any>,
+    newValue: HeaderEnum
+  ) => {
+    setCurrentPath(newValue);
+  };
+
   return (
     <Box component={'header'} className="header-outer-box">
       <Box className="header-inner-box">
-        <Box>
-          <Link href="/" className="header-title">
-            OpenBC
-          </Link>
-        </Box>
-        <Tabs value={false}>
-          <Tab
-            label="Projects"
-            value="/projects"
-            href="/projects"
-            role="button"
-            className="header-tab"
-            component={Link}
-          />
-          <Tab
-            label="Join Us"
-            value="/join-us"
-            href="/join-us"
-            role="button"
-            className="header-tab"
-            component={Link}
-          />
-          <Tab
-            label="About"
-            value="/about"
-            href="/about"
-            role="button"
-            className="header-tab"
-            component={Link}
-          />
+        <Link href="/" className="title-link">
+          OpenBC
+        </Link>
+        <Tabs value={currentPath} onChange={handleChange}>
+          {headerData.map((tab, index) => (
+            <Tab
+              key={index}
+              label={tab.label}
+              value={tab.value}
+              href={tab.href}
+              role="button"
+              className="header-tab"
+              component={Link}
+            />
+          ))}
         </Tabs>
         <Box component={'nav'} className="header-nav"></Box>
       </Box>
