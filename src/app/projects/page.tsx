8a2cff155bbf2a37d31017191172f053429b580f
Nam Anh Mai <maithenamanh@gmail.com>
2024-01-11 15:45:52 -0800

    Change to server component to fetch projects data

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -8,16 +6,19 @@ import CardMedia from '@mui/material/CardMedia';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
-import * as React from 'react';
+
+import { getLocalProjectData } from '@/utils/get-local-project-data/get-local-project-data';
+import { Project as ProjectInterface } from '@/app/dataModels/project';
 import './projects.scss';
+import theme from '@/theme';
 
-const cards = [1, 2, 3, 4, 5, 6];
+export default async function Projects() {
+  const localProjectData = await getLocalProjectData();
 
-export default function Album() {
   return (
-    <ThemeProvider theme={createTheme()}>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <main>
         <h1 className="centered-elements">Projects</h1>
@@ -25,8 +26,8 @@ export default function Album() {
         <Container className="projects-container" maxWidth="md">
           {/* End hero unit */}
           <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
+            {localProjectData.map((project: ProjectInterface) => (
+              <Grid item key={project.title} xs={12} sm={6} md={4}>
                 <Card className="card">
                   <CardMedia
                     component="div"
