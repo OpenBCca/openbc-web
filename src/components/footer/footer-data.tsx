f1592d9b95f7d6d32aa15813a99794bab1657fd2
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2024-01-23 18:33:41 -0800

    footer created

--- src/components/footer/footer-data.tsx
+++
@@ -1,9 +0,0 @@
-import HeaderEnum from '../../enum/header-enum';
-
-const footerData = [
-  { label: 'Projects', value: HeaderEnum.projects, href: '/projects' },
-  { label: 'Join Us', value: HeaderEnum.joinUs, href: '/join-us' },
-  { label: 'About', value: HeaderEnum.about, href: '/about' },
-];
-
-export default footerData;
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,146 +4,85 @@ import {
   Button,
   Container,
   Grid,
-  Stack,
-  TextField,
+  IconButton,
   Typography,
+  useMediaQuery,
 } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 import Image from 'next/image';
-import { usePathname } from 'next/navigation';
-
-import React from 'react';
+import Link from 'next/link';
 import './footer.scss';
 
 function Footer() {
-  const pathname = usePathname();
-
-  const isMenuItemActive = (href: string) => {
-    return pathname === href;
-  };
-
+  const theme = useTheme();
+  const mobileBreakpoint = useMediaQuery(theme.breakpoints.down('md'));
   return (
-    <Box className="footer-container">
-      <Container className="footer" maxWidth="lg">
-        <Grid container className="footer__content-container">
+    <Box component="footer" className="footer-container">
+      <Container>
+        <Grid container spacing={2} className="footer">
+          <Grid item xs={12} md={4} className="footer__text-container">
+            <Link href="#" className="footer__link">
+              <Typography variant="h6">OpenBC</Typography>
+            </Link>
+
+            <Typography variant="body2">
+              Volunteer-driven community for positive impact in British Columbia
+            </Typography>
+          </Grid>
           <Grid
             item
             xs={12}
-            md={6}
-            className="footer__menu-container"
-            direction="column"
+            md={4}
+            className={mobileBreakpoint ? '' : 'footer__nav-container'}
           >
-            <Typography variant="h6" className="footer__logo">
-              OpenBC
-            </Typography>
-            <Stack className="footer__menu" direction="row" spacing={2}>
-              <Button
-                href="/about"
-                size="small"
-                className={`footer__menu-item ${
-                  isMenuItemActive('/about') ? 'footer__menu-item--active' : ''
-                }`}
-                variant="text"
-              >
-                About
-              </Button>
-              <Button
-                href="/join-us"
-                size="small"
-                className={`footer__menu-item ${
-                  isMenuItemActive('/join-us')
-                    ? 'footer__menu-item--active'
-                    : ''
-                }`}
-                variant="text"
-              >
-                Join Us
-              </Button>
-              <Button
-                href="/contact-us"
-                size="small"
-                className={`footer__menu-item ${
-                  isMenuItemActive('/contact-us')
-                    ? 'footer__menu-item--footer__menu-item--active'
-                    : ''
-                }`}
-                variant="text"
-              >
-                Contact Us
-              </Button>
-            </Stack>
-          </Grid>
-          <Grid className="footer__form" item xs={12} md={6}>
-            <Stack
-              spacing={2}
-              className="footer__form-group"
-              direction="column"
-            >
-              <Typography variant="subtitle1" className="footer__form-label">
-                Get the latest updates about OpenBC
-              </Typography>
-              <TextField
-                className="footer__form-input"
-                variant="outlined"
-                fullWidth
-                size="small"
-              />
-            </Stack>
-            <Button className="footer__form-submit" variant="outlined">
-              Subscribe
+            <Button variant="text" size="small" href="/projects">
+              Projects
+            </Button>
+            <Button variant="text" size="small" href="/join-us">
+              Join Us
+            </Button>
+            <Button variant="text" size="small" href="/about">
+              About
             </Button>
           </Grid>
-        </Grid>
-
-        <Grid
-          container
-          className="footer__content-container footer__content-container--border"
-        >
           <Grid
-            className="footer__icon-container"
-            direction="row"
-            alignItems="center"
-            spacing={2}
+            item
             xs={12}
-            md={6}
+            md={4}
+            className={mobileBreakpoint ? '' : 'footer__link-container'}
           >
-            <Image
-              alt="linkedin"
-              src="/assets/linkedin_Icon.svg"
-              className="footer__icon"
-              width={20}
-              height={20}
-            />
-            <Image
-              alt="github"
-              src="/assets/github_Icon.svg"
-              className="footer__icon"
-              width={20}
-              height={20}
-            />
-            <Image
-              alt="discord"
-              src="/assets/discord_Icon.svg"
-              className="footer__icon"
-              width={20}
-              height={20}
-            />
-          </Grid>
-          <Grid
-            xs={12}
-            md={6}
-            className="footer__terms-container"
-            direction="row"
-            alignItems="center"
-            spacing={4}
-          >
-            <Typography variant="body2" className="footer__terms">
-              Terms of Service
-            </Typography>
-            <Typography variant="body2" className="footer__terms">
-              Privacy Policy
-            </Typography>
+            <IconButton
+              href="https://github.com/OpenBCca"
+              aria-label="GitHub Account"
+            >
+              <Image
+                src="/assets/github_Icon.svg"
+                alt="github account"
+                width={20}
+                height={20}
+              />
+            </IconButton>
+            <IconButton
+              href="https://www.linkedin.com/company/openbc/"
+              aria-label="LinkedIn Account"
+            >
+              <Image
+                src="/assets/linkedin_Icon.svg"
+                alt="Linkedin account"
+                width={20}
+                height={20}
+              />
+            </IconButton>
           </Grid>
         </Grid>
+        <Box className="copyright">
+          <Typography variant="body2" className="copyright__text">
+            <Link href="#" className="footer__link">
+              <strong>OpenBC</strong>
+            </Link>{' '}
+            © 2024 All Rights Reserved
+          </Typography>
+        </Box>
       </Container>
     </Box>
   );
