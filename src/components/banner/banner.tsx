cff2d9fda5cd77924382780c641e40b3a95dd990
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2024-01-31 18:48:41 -0800

    working toward banner

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -1,19 +1,55 @@
 'use client';
 
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Container, Typography } from '@mui/material';
+import { Abhaya_Libre, Lato } from 'next/font/google';
 import React from 'react';
 import './banner.scss';
 
+const abhayaLibre = Abhaya_Libre({
+  subsets: ['latin'],
+  weight: ['800'],
+});
+
+const lato = Lato({
+  subsets: ['latin'],
+  weight: ['400'],
+});
+
 function Banner() {
   return (
-    <Box className="banner">
-      <Typography variant="h4" className="banner-title">
-        {'Be a Force For Good in BC!'}
-      </Typography>
-      <Typography variant="body1">
-        {'Do you have design, writing, or coding skills? You have the power to elevate British Columbia to new heights\n' +
-          '        through OpenBC.'}
-      </Typography>
+    <Box className="banner-container">
+      <Container className="banner">
+        <Box className="banner__text-container">
+          <Typography
+            variant="h1"
+            className={`${abhayaLibre.className} banner__title`}
+          >
+            {'Be a Force For Good in BC!'}
+          </Typography>
+          <Typography
+            variant="body1"
+            className={`${lato.className} banner__text`}
+          >
+            Do you have design, writing, or coding skills? You have the power to
+            elevate British Columbia to new heights through OpenBC.
+          </Typography>
+          <Box className="banner__button-container">
+            <Button
+              className={`${lato.className} banner__button`}
+              variant="outlined"
+            >
+              Become A Partner
+            </Button>
+            <Button
+              className={`${lato.className} banner__button`}
+              variant="contained"
+            >
+              Join Us
+            </Button>
+          </Box>
+        </Box>
+        <Box className="banner__image-container"></Box>
+      </Container>
     </Box>
   );
 }
