4561b33b1b73a6df6114fac81d61d9f5c2235157
Pooria <pooria_taghdiri@hotmail.com>
2024-02-04 21:46:34 -0800

    Features/create join us (#90)
    Co-authored-by: Ahmad Akhtar <105146344+Unknown-0perator@users.noreply.github.com>

--- src/app/join-us/join-us.page.test.tsx
+++ src/app/join-us/join-us.page.test.tsx
@@ -1,12 +1,12 @@
+import JoinUs from '@/app/join-us/page';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import JoinUs from '@/app/join-us/page';
 
 describe('Join Us Page', () => {
   it('should render title', () => {
     render(<JoinUs />);
-    const title = screen.getByText('Join Us');
-    expect(title).toBeInTheDocument();
+    const subTitle = screen.getByRole('heading', { name: 'Our Mission' });
+    expect(subTitle).toBeInTheDocument();
   });
 });
--- src/app/join-us/page.tsx
+++ src/app/join-us/page.tsx
@@ -1,32 +1,154 @@
-import { Typography } from '@mui/material';
+import CoreValues from '@/components/core-values/core-values';
+import Faq from '@/components/faq/faq';
+import { Box, Divider, Link, Paper, Typography } from '@mui/material';
 import Container from '@mui/material/Container';
+import Image from 'next/image';
 import React from 'react';
 import './join-us.scss';
 
 export default function JoinUs() {
   return (
     <Container maxWidth="lg" className="container">
-      <Typography className="join-us-header" variant="h3">
-        Join Us
+      <Box className="sub-container-2-col">
+        <Box className="box-size-50">
+          <Image
+            src="https://source.unsplash.com/random?wallpapers"
+            alt="Description"
+            width={500}
+            height={500}
+            style={{ width: '100%' }}
+          />
+        </Box>
+        <Box className="box-size-50__marginX">
+          <Typography className="sub-header" variant="h2">
+            Our Mission
+          </Typography>
+          <Typography variant="body2" paragraph>
+            At OpenBC, we&apos;re passionate about leveraging your unique
+            abilities for the advancement of our province.
+          </Typography>
+          <Typography variant="body2" paragraph>
+            <strong>
+              Contribute to Open Source Projects for BC, from Anywhere, with
+              OpenBC.
+            </strong>
+          </Typography>
+        </Box>
+      </Box>
+      <Box className="margin-left-4 margin-bottom-4">
+        <Typography className="sub-header" variant="h5">
+          Visions
+        </Typography>
+        <Typography variant="body2" paragraph>
+          Quis neque, eu et ipsum amet, vel et. Varius integer enim pellentesque
+          ornare pharetra faucibs arcu. Mauris blandit egestas nibh. eu et ipsum
+          amet, vel et. Varius integer enim pellentesque ornare pharetra faucibs
+          arcu. Mauris blandit egestas nibh.
+        </Typography>
+      </Box>
+      <Box className="box-size-40__center">
+        <Box className="display-flex__space-around">
+          <Link href="#" className="link-no-decorated" variant="h5">
+            Volunteers
+          </Link>
+          <Link href="#" className="link-no-decorated" variant="h5">
+            Be A Partner
+          </Link>
+        </Box>
+        <Divider className="divider" />
+      </Box>
+      <Box className="sub-container-2-col">
+        <Box className="box-size-50">
+          <Typography className="sub-header" variant="h5">
+            <strong>
+              Ready to contribute to British Columbia through Open Source
+              projects?
+            </strong>
+          </Typography>
+          <Typography variant="body2" paragraph>
+            Join us at OpenBC and become a part of our active and welcoming
+            community.
+          </Typography>
+        </Box>
+        <Box className="box-size-50">
+          <Box className="display-flex">
+            <Paper className="square">
+              <Box className="box-number">
+                <Typography>1</Typography>
+              </Box>
+            </Paper>
+            <Typography className="margin-left-1" variant="h5">
+              Tell Us About Yourself
+            </Typography>
+          </Box>
+          <Typography className="margin-left-4" variant="body2" paragraph>
+            Fill out our <u>2 minute form</u> so we can help match you with
+            opportunities
+          </Typography>
+          <Box className="display-flex">
+            <Paper className="square">
+              <Box className="box-number">
+                <Typography>2</Typography>
+              </Box>
+            </Paper>
+            <Typography className="margin-left-1" variant="h5">
+              Onboarding
+            </Typography>
+          </Box>
+          <Typography className="margin-left-4" variant="body2" paragraph>
+            Tortor interdumd pulvinar risus. Cursus in odio aenean.
+          </Typography>
+          <Box className="display-flex">
+            <Paper className="square">
+              <Box className="box-number">
+                <Typography>3</Typography>
+              </Box>
+            </Paper>
+            <Typography className="margin-left-1" variant="h5">
+              Have a question?
+            </Typography>
+          </Box>
+          <Typography className="margin-left-4" variant="body2" paragraph>
+            Find you answers in FAQ or email{' '}
+            <Link href="mailto:&#x6f;&#x70;&#x65;&#x6e;&#x62;&#x63;&#x2e;&#x63;&#x61;&#x40;&#x67;&#x6d;&#x61;&#x69;&#x6c;&#x2e;&#x63;&#x6f;&#x6d;">
+              openbc.ca AT gmail.com
+            </Link>{' '}
+            with any questions or feedback.
+          </Typography>
+        </Box>
+      </Box>
+      <Typography className="sub-header" variant="h5">
+        Core Values
       </Typography>
-      <Typography variant="body2" paragraph>
-        Unlock the power of your skills and expertise to drive positive change
-        in British Columbia through OpenBC. At OpenBC, we&apos;re passionate
-        about leveraging your unique abilities for the advancement of our
-        province. Whether you&apos;re a tech enthusiast, a creative visionary,
-        or a community leader, your skills can play a crucial role in addressing
-        challenges and creating opportunities.
-      </Typography>
-      <Typography variant="body2" paragraph>
-        Ready to contribute to British Columbia through Open source projects?
-        Join us at OpenBC and become a part of our active and welcoming
-        community. Here&apos;s how you can engage:
-      </Typography>
-      <Typography variant="body2">
-        <strong>
-          Email openbc.ca@gmail.com for opportunities to contribute.
-        </strong>
-      </Typography>
+      <Box className="sub-container-2-col">
+        <Box className="box-size-70">
+          <CoreValues />
+          <CoreValues />
+          <CoreValues />
+          <CoreValues />
+          <CoreValues />
+        </Box>
+        <Box className="box-size-30">
+          <Image
+            src="https://source.unsplash.com/random?wallpapers"
+            alt="Description"
+            width={500}
+            height={500}
+            style={{ width: '80%', height: '80%' }}
+          />
+        </Box>
+      </Box>
+      <Box>
+        <Box className="display-flex__center">
+          <Typography className="sub-header" variant="h5">
+            Frequently Asked Question (FAQ)
+          </Typography>
+        </Box>
+        <Faq />
+        <Faq />
+        <Faq />
+        <Faq />
+      </Box>
     </Container>
   );
 }
