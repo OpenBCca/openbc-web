67334a6598d54a9f9b8d83f41648567645591a5e
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2024-02-06 19:18:43 -0800

    HTML entities for single quotes

--- src/components/faq/faq.tsx
+++ src/components/faq/faq.tsx
@@ -52,12 +52,12 @@ export default function Faq() {
           </AccordionSummary>
           <AccordionDetails>
             <Typography className="faq__answer">
-              At OpenBC, we're a mix of seasoned pros and eager learners, all
-              excited about trying new stuff. Feel free to use your know-how to
-              speed things up and help the team, but remember, we're all about
-              diving into new things here. If you're picking up a new role, just
-              a heads up: you might need to put in some extra time on your own
-              to get the hang of it and get your tasks done.
+              At OpenBC, we&apos;re a mix of seasoned pros and eager learners,
+              all excited about trying new stuff. Feel free to use your know-how
+              to speed things up and help the team, but remember, we&apos;re all
+              about diving into new things here. If you&apos;re picking up a new
+              role, just a heads up: you might need to put in some extra time on
+              your own to get the hang of it and get your tasks done.
             </Typography>
           </AccordionDetails>
         </Accordion>
@@ -78,12 +78,12 @@ export default function Faq() {
           </AccordionSummary>
           <AccordionDetails>
             <Typography className="faq__answer">
-              At OpenBC, we're a mix of seasoned pros and eager learners, all
-              excited about trying new stuff. Feel free to use your know-how to
-              speed things up and help the team, but remember, we're all about
-              diving into new things here. If you're picking up a new role, just
-              a heads up: you might need to put in some extra time on your own
-              to get the hang of it and get your tasks done.
+              At OpenBC, we&apos;re a mix of seasoned pros and eager learners,
+              all excited about trying new stuff. Feel free to use your know-how
+              to speed things up and help the team, but remember, we&apos;re all
+              about diving into new things here. If you&apos;re picking up a new
+              role, just a heads up: you might need to put in some extra time on
+              your own to get the hang of it and get your tasks done.
             </Typography>
           </AccordionDetails>
         </Accordion>
@@ -104,12 +104,12 @@ export default function Faq() {
           </AccordionSummary>
           <AccordionDetails>
             <Typography className="faq__answer">
-              At OpenBC, we're a mix of seasoned pros and eager learners, all
-              excited about trying new stuff. Feel free to use your know-how to
-              speed things up and help the team, but remember, we're all about
-              diving into new things here. If you're picking up a new role, just
-              a heads up: you might need to put in some extra time on your own
-              to get the hang of it and get your tasks done.
+              At OpenBC, we&apos;re a mix of seasoned pros and eager learners,
+              all excited about trying new stuff. Feel free to use your know-how
+              to speed things up and help the team, but remember, we&apos;re all
+              about diving into new things here. If you&apos;re picking up a new
+              role, just a heads up: you might need to put in some extra time on
+              your own to get the hang of it and get your tasks done.
             </Typography>
           </AccordionDetails>
         </Accordion>
@@ -130,12 +130,12 @@ export default function Faq() {
           </AccordionSummary>
           <AccordionDetails>
             <Typography className="faq__answer">
-              At OpenBC, we're a mix of seasoned pros and eager learners, all
-              excited about trying new stuff. Feel free to use your know-how to
-              speed things up and help the team, but remember, we're all about
-              diving into new things here. If you're picking up a new role, just
-              a heads up: you might need to put in some extra time on your own
-              to get the hang of it and get your tasks done.
+              At OpenBC, we&apos;re a mix of seasoned pros and eager learners,
+              all excited about trying new stuff. Feel free to use your know-how
+              to speed things up and help the team, but remember, we&apos;re all
+              about diving into new things here. If you&apos;re picking up a new
+              role, just a heads up: you might need to put in some extra time on
+              your own to get the hang of it and get your tasks done.
             </Typography>
           </AccordionDetails>
         </Accordion>
@@ -156,12 +156,12 @@ export default function Faq() {
           </AccordionSummary>
           <AccordionDetails>
             <Typography className="faq__answer">
-              At OpenBC, we're a mix of seasoned pros and eager learners, all
-              excited about trying new stuff. Feel free to use your know-how to
-              speed things up and help the team, but remember, we're all about
-              diving into new things here. If you're picking up a new role, just
-              a heads up: you might need to put in some extra time on your own
-              to get the hang of it and get your tasks done.
+              At OpenBC, we&apos;re a mix of seasoned pros and eager learners,
+              all excited about trying new stuff. Feel free to use your know-how
+              to speed things up and help the team, but remember, we&apos;re all
+              about diving into new things here. If you&apos;re picking up a new
+              role, just a heads up: you might need to put in some extra time on
+              your own to get the hang of it and get your tasks done.
             </Typography>
           </AccordionDetails>
         </Accordion>
