b5cac96386d30202f1e2314f00fc01c830107b4a
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2024-02-17 22:31:16 -0800

    footer styling bug

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,29 +1,37 @@
 'use client';
 
+import CloseIcon from '@mui/icons-material/Close';
 import {
   Box,
   Button,
   Container,
   IconButton,
   Link,
+  Modal,
   Typography,
 } from '@mui/material';
 import Image from 'next/image';
+import { useState } from 'react';
 import './footer.scss';
 
 function Footer() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <Box component="footer" className="footer-container">
-      <Container className="footer">
+      <Container className="footer" sx={{ display: 'flex' }}>
         <Box className="footer__wrapper">
           <Box className="footer__menu">
-            <Image
-              src="/assets/OpenBC-logo.svg"
-              width={48}
-              height={48}
-              alt="OpenBC logo"
-              className="footer__logo"
-            />
+            <Link href="/">
+              <Image
+                src="/assets/OpenBC-logo.svg"
+                width={48}
+                height={48}
+                alt="OpenBC logo"
+                className="footer__logo"
+              />
+            </Link>
             <Typography className="footer__button">
               <Link
                 underline="none"
@@ -65,7 +73,13 @@ function Footer() {
                 placeholder="Enter your Email."
               />
 
-              <Button className="footer__subscribe-button">Subscribe</Button>
+              <Button
+                onClick={handleOpen}
+                className="footer__subscribe-button"
+                variant="outlined"
+              >
+                Subscribe
+              </Button>
             </Box>
           </form>
         </Box>
@@ -97,16 +111,44 @@ function Footer() {
             </IconButton>
           </Box>
           <Box className="footer__links">
-            <Typography className="footer__link">
-              <Link href="#" color="inherit" underline="none">
-                Terms of Service
-              </Link>
-            </Typography>
-            <Typography className="footer__link">
-              <Link href="#" color="inherit" underline="none">
-                Privacy Policy
-              </Link>
-            </Typography>
+            <button onClick={handleOpen} className="footer__link">
+              Terms of Service
+            </button>
+            <button onClick={handleOpen} className="footer__link">
+              Privacy Policy
+            </button>
+
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box className="modal">
+                <IconButton
+                  onClick={handleClose}
+                  className="modal__close"
+                  color="error"
+                >
+                  <CloseIcon />
+                </IconButton>
+                <Typography
+                  className="modal__title"
+                  variant="h6"
+                  component="h2"
+                >
+                  OOPS
+                </Typography>
+                <Typography
+                  className="modal__text"
+                  variant="body2"
+                  component="h2"
+                >
+                  We&apos;re actively building this feature to make your
+                  experience even better.
+                </Typography>
+              </Box>
+            </Modal>
           </Box>
         </Box>
       </Container>
